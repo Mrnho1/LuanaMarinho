cf89a54a8eaeb760a2c95dc13f079400074f9da2
lucas <ls.marinho36@gmail.com>
2025-04-27 16:47:02 -0300

    add animation.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        }
+      });
+    }, {
+      threshold: 0.1
+    });
+  
+    const elements = document.querySelectorAll('.fade-in-on-scroll');
+    elements.forEach(el => observer.observe(el));
+  });
+
 document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".menu-toggle");
     const navLinks = document.querySelector(".nav-links");
@@ -6,3 +21,4 @@ document.addEventListener("DOMContentLoaded", function () {
         navLinks.classList.toggle("nav-active");
     });
 });
+
